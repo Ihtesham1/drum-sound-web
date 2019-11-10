3f243d567e2c045549f9655c96335f32e9147002
Ihtesham <qazi.m.ihtesham@gmail.com>
2019-11-10 20:37:14 +0500

    Sounds working on click

--- index.js
+++ index.js
@@ -6,7 +6,41 @@ for (var i = 0; i < numOfDrumButton ; i ++){
 document.querySelectorAll("button")[i].addEventListener("click", clickWorking)
 
 function clickWorking(){
-    alert("I got clicked")
+    var alpha = this.innerHTML
+
+    if(alpha == 'w'){
+        var audio = new Audio('sounds/tom-1.mp3');
+        audio.play();
+    }
+    else if(alpha == 'a'){
+        var audio = new Audio('sounds/tom-2.mp3');
+        audio.play();
+    }
+    else if(alpha == 's'){
+        var audio = new Audio('sounds/tom-3.mp3');
+        audio.play();
+    }
+    else if(alpha == 'd'){
+        var audio = new Audio('sounds/tom-4.mp3');
+        audio.play();
+    }
+    else if(alpha == 'j'){
+        var audio = new Audio('sounds/crash.mp3');
+        audio.play();
+    }
+    else if(alpha == 'k'){
+        var audio = new Audio('sounds/kick-bass.mp3');
+        audio.play();
+    }
+    else if(alpha == 'l'){
+        var audio = new Audio('sounds/snare.mp3');
+        audio.play();
+    }
+
+
+    
+
+    //alert("I got clicked")
 }
 
 }
