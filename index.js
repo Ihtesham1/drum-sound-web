31ea11f5fff180a5e6263d569306d2d8f9528174
Ihtesham <qazi.m.ihtesham@gmail.com>
2019-11-16 13:21:43 +0500

    sound working on keys and clicks

--- index.js
+++ index.js
@@ -2,45 +2,48 @@ var numOfDrumButton = document.querySelectorAll(".drum").length
 
 for (var i = 0; i < numOfDrumButton ; i ++){
 
-    
-document.querySelectorAll("button")[i].addEventListener("click", clickWorking)
 
-function clickWorking(){
-    var alpha = this.innerHTML
+document.querySelectorAll("button")[i].addEventListener("click", function() {
 
-    if(alpha == 'w'){
+    var cil = this.innerHTML
+    makeSound(cil)
+
+})
+
+document.addEventListener("keypress", function(event) {
+    var keypad = event.key
+    makeSound(keypad)
+})
+
+function makeSound(sound){
+    if(sound == 'w'){
         var audio = new Audio('sounds/tom-1.mp3');
         audio.play();
     }
-    else if(alpha == 'a'){
+    else if(sound == 'a'){
         var audio = new Audio('sounds/tom-2.mp3');
         audio.play();
     }
-    else if(alpha == 's'){
+    else if(sound == 's'){
         var audio = new Audio('sounds/tom-3.mp3');
         audio.play();
     }
-    else if(alpha == 'd'){
+    else if(sound == 'd'){
         var audio = new Audio('sounds/tom-4.mp3');
         audio.play();
     }
-    else if(alpha == 'j'){
+    else if(sound == 'j'){
         var audio = new Audio('sounds/crash.mp3');
         audio.play();
     }
-    else if(alpha == 'k'){
+    else if(sound == 'k'){
         var audio = new Audio('sounds/kick-bass.mp3');
         audio.play();
     }
-    else if(alpha == 'l'){
+    else if(sound == 'l'){
         var audio = new Audio('sounds/snare.mp3');
         audio.play();
     }
-
-
-    
-
-    //alert("I got clicked")
 }
 
 }
